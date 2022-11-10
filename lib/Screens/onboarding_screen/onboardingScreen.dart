89bdb2dc406d75b26e269e11ca06b5b9ab758d96
Femi Fashola <Fashola436@gmail.com>
2022-11-10 21:53:09 +0100

    new

--- lib/Screens/onboarding_screen/onboardingScreen.dart
+++ lib/Screens/onboarding_screen/onboardingScreen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:gofarmng/Constants/size_config.dart';
 import 'package:gofarmng/Styles/colors.dart';
 
 import '../../Constants/size_config.dart';
