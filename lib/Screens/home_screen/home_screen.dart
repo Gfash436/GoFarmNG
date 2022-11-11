6fc5e163f6f4e69a267d53de418109e38ee4d93e
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-11-11 09:21:06 +0100

    update

--- lib/Screens/home_screen/home_screen.dart
+++ lib/Screens/home_screen/home_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gofarmng/Constants/size_config.dart';
 import 'package:gofarmng/Widgets/myText.dart';
 
-import '../../Widgets/textField.dart';
+import '../../Styles/colors.dart';
+import '../../Widgets/search_textformfield.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,35 +15,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController searchController = TextEditingController();
+
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/menu.svg",
-          ),
-          onPressed: () {},
-        ),
-        actions: [
-          SvgPicture.asset(
-            "assets/icons/notification.svg",
-          ),
-          SizedBox(
-            width: getProportionateScreenWidth(20),
-          ),
-          SvgPicture.asset(
-            "assets/icons/cart.svg",
-          ),
-          SizedBox(
-            width: getProportionateScreenWidth(20),
-          ),
-        ],
-      ),
+      appBar: appBar(),
       body: Padding(
         padding: EdgeInsets.only(
           left: getProportionateScreenWidth(20),
@@ -62,92 +42,93 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SearchTextFormField extends StatelessWidget {
-  const SearchTextFormField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: getProportionateScreenWidth(
-        335,
-      ),
-      height: getProportionateScreenHeight(
-        40,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: getProportionateScreenWidth(
-              290,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (value) => setState(() {
+          _selectedIndex = value;
+        }),
+        selectedItemColor: green,
+        unselectedItemColor: grey,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(
+          fontSize: getProportionateScreenWidth(10),
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontSize: getProportionateScreenWidth(10),
+          fontWeight: FontWeight.normal,
+        ),
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/home.svg",
+              color: grey,
             ),
-            child: TextFormField(
-              textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0xfff6f6f6),
-                hintText: "Search Products...",
-                hintStyle: TextStyle(
-                  fontSize: getProportionateScreenHeight(
-                    13,
-                  ),
-                  fontWeight: FontWeight.w400,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
-                prefix: SizedBox(
-                    height: 24,
-                    child: VerticalDivider(
-                      color: Colors.red,
-                      width: getProportionateScreenHeight(.5),
-                    )),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: SvgPicture.asset(
-                    "assets/icons/search.svg",
-                    height: getProportionateScreenHeight(
-                      16,
-                    ),
-                    width: getProportionateScreenWidth(
-                      16,
-                    ),
-                  ),
-                ),
-              ),
+            label: "Home",
+            activeIcon: SvgPicture.asset(
+              "assets/icons/home.svg",
             ),
           ),
-          SizedBox(
-            height: getProportionateScreenHeight(
-              40,
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/analytics.svg",
+              color: grey,
             ),
-            width: getProportionateScreenHeight(
-              37,
+            label: "Analytics",
+            activeIcon: SvgPicture.asset(
+              "assets/icons/analytics.svg",
             ),
-            child: Container(
-              padding: EdgeInsets.all(
-                getProportionateScreenWidth(
-                  8,
-                ),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  8,
-                ),
-                color: const Color(0xfff6f6f6),
-              ),
-              child: SvgPicture.asset(
-                "assets/icons/filter.svg",
-              ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/favorite.svg",
+              color: grey,
+            ),
+            label: "Favorite",
+            activeIcon: SvgPicture.asset(
+              "assets/icons/favorite.svg",
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/wallet.svg",
+              color: grey,
+            ),
+            label: "Wallet",
+            activeIcon: SvgPicture.asset(
+              "assets/icons/wallet.svg",
             ),
           ),
         ],
       ),
     );
   }
+}
+
+AppBar appBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset(
+        "assets/icons/menu.svg",
+      ),
+      onPressed: () {},
+    ),
+    actions: [
+      SvgPicture.asset(
+        "assets/icons/notification.svg",
+      ),
+      SizedBox(
+        width: getProportionateScreenWidth(20),
+      ),
+      SvgPicture.asset(
+        "assets/icons/cart.svg",
+      ),
+      SizedBox(
+        width: getProportionateScreenWidth(20),
+      ),
+    ],
+  );
 }
