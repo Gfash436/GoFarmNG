39014b6aa12602473ea76e9bb15038a4c0645f70
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-11-12 06:14:03 +0100

    navBar update

--- lib/Screens/home_screen/home_screen.dart
+++ lib/Screens/home_screen/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:gofarmng/Widgets/myText.dart';
 import '../../Styles/colors.dart';
 import '../../Widgets/search_textformfield.dart';
 import 'app_bar.dart';
+import 'bottom_navBar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,8 +17,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController searchController = TextEditingController();
-
-  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
@@ -122,10 +121,10 @@ class _HomeScreenState extends State<HomeScreen> {
               decoration: BoxDecoration(
                 boxShadow: [
                   BoxShadow(
-                    color: Color(0xff111111).withOpacity(
+                    color: const Color(0xff111111).withOpacity(
                       .25,
                     ),
-                    offset: Offset(
+                    offset: const Offset(
                       0,
                       1,
                     ),
@@ -138,91 +137,131 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 color: Colors.white,
               ),
-              child: Column(
+              child: Stack(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle, color: mainlightGrey),
-                        child: SvgPicture.asset("assets/icons/love.svg"),
-                        padding: EdgeInsets.all(
-                          8,
+                  SizedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: mainlightGrey),
+                          padding: const EdgeInsets.all(
+                            8,
+                          ),
+                          child: SvgPicture.asset(
+                            "assets/icons/love.svg",
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: getProportionateScreenHeight(96),
+                    // width: getProportionateScreenWidth(140),
+                    left: getProportionateScreenHeight(20),
+                    right: getProportionateScreenHeight(20),
+                    child: Image.asset(
+                      "assets/images/banana.png",
+                    ),
+                  ),
+                  Positioned(
+                    bottom: getProportionateScreenHeight(72),
+                    width: getProportionateScreenWidth(75),
+                    // left: getProportionateScreenWidth(32.5),
+                    right: getProportionateScreenWidth(18.5),
+                    child: Text("Fresh Banana",
+                        // maxLines: 1,
+                        softWrap: true,
+                        style: TextStyle(
+                          fontSize: getProportionateScreenWidth(
+                            12,
+                          ),
+                          fontWeight: FontWeight.w500,
+                        )),
+                  ),
+                  Positioned(
+                    left: getProportionateScreenWidth(40),
+                    // right: getProportionateScreenWidth(25),
+                    // top: getProportionateScreenHeight(126.57),
+                    bottom: getProportionateScreenHeight(48),
+                    // width: getProportionateScreenWidth(36),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        myText(
+                          text: "4.5",
+                          fontSize: getProportionateScreenWidth(
+                            11,
+                          ),
+                          fontWeight: FontWeight.w400,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                          child: SvgPicture.asset(
+                            "assets/icons/star.svg",
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // SizedBox(
+                  //   height: getProportionateScreenHeight(
+                  //     8,
+                  //   ),
+                  // ),
+                  Positioned(
+                    bottom: 0,
+                    width: getProportionateScreenWidth(108),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            text: "N1,000",
+                            style: TextStyle(
+                              fontSize: getProportionateScreenWidth(
+                                12,
+                              ),
+                              fontWeight: FontWeight.w400,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "/Kg",
+                                style: TextStyle(
+                                  fontSize: getProportionateScreenWidth(
+                                    8,
+                                  ),
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: const Color(0xffE5F7E1),
+                          ),
+                          child: const Icon(
+                            Icons.add,
+                            color: Color(0xff353535),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
-      bottomNavigationBar: bottomNavBar(),
-    );
-  }
-
-  BottomNavigationBar bottomNavBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: _selectedIndex,
-      onTap: (value) => setState(() {
-        _selectedIndex = value;
-      }),
-      selectedItemColor: green,
-      unselectedItemColor: grey,
-      showUnselectedLabels: true,
-      selectedLabelStyle: TextStyle(
-        fontSize: getProportionateScreenWidth(10),
-        fontWeight: FontWeight.bold,
-      ),
-      unselectedLabelStyle: TextStyle(
-        fontSize: getProportionateScreenWidth(10),
-        fontWeight: FontWeight.normal,
-      ),
-      items: [
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            "assets/icons/home.svg",
-            color: grey,
-          ),
-          label: "Home",
-          activeIcon: SvgPicture.asset(
-            "assets/icons/home.svg",
-          ),
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            "assets/icons/analytics.svg",
-            color: grey,
-          ),
-          label: "Analytics",
-          activeIcon: SvgPicture.asset(
-            "assets/icons/analytics.svg",
-          ),
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            "assets/icons/favorite.svg",
-            color: grey,
-          ),
-          label: "Favorite",
-          activeIcon: SvgPicture.asset(
-            "assets/icons/favorite.svg",
-          ),
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            "assets/icons/wallet.svg",
-            color: grey,
-          ),
-          label: "Wallet",
-          activeIcon: SvgPicture.asset(
-            "assets/icons/wallet.svg",
-          ),
-        ),
-      ],
+      bottomNavigationBar: const bottomNavBar(),
     );
   }
 }
