ac6c8287efcb7e1211e936971f48942789b5e72e
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-11-13 12:32:04 +0100

    favorite completed

--- lib/Screens/favorite_screen/favorite_screen.dart
+++ lib/Screens/favorite_screen/favorite_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gofarmng/Constants/size_config.dart';
 
+import '../../Widgets/search_textformfield.dart';
 import '../home_screen/app_bar.dart';
 import '../home_screen/app_drawer.dart';
+import 'fovorite_components.dart';
 
 class FavoriteScreen extends StatelessWidget {
   FavoriteScreen({super.key});
@@ -14,6 +17,33 @@ class FavoriteScreen extends StatelessWidget {
       key: scafoldKey,
       appBar: appBar(context, scafoldKey),
       drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(16),
+        ),
+        child: Column(
+          children: [
+            const SearchTextFormField(),
+            SizedBox(
+              height: getProportionateScreenHeight(
+                16,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      ...List.generate(
+                        15,
+                        (index) => const FavComponents(),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
