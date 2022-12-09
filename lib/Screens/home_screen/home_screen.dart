be6ae8f9b7eec0a4bb7b9ee94e8dd5da80204541
Abdulganiyu Sulyman <suitpop2021@gmail.com>
2022-12-09 10:19:11 +0100

    update

--- lib/Screens/home_screen/home_screen.dart
+++ lib/Screens/home_screen/home_screen.dart
@@ -1,16 +1,18 @@
-// import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:gofarmng/Constants/size_config.dart';
 import 'package:gofarmng/Screens/analytics_screen/analytics_screen.dart';
 import 'package:gofarmng/Screens/favorite_screen/favorite_screen.dart';
 import 'package:gofarmng/Screens/wallet_screen/wallet_screen.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:gofarmng/Widgets/myText.dart';
 
+import '../../Widgets/search_textformfield.dart';
 import 'app_bar.dart';
 import 'app_drawer.dart';
 import 'bottom_navBar.dart';
 import 'home_screen_body.dart';
+import 'newest_arrival.dart';
+import 'popular_categories.dart';
+import 'top_selling_products.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,16 +25,12 @@ class _HomeScreenState extends State<HomeScreen> {
   // final TextEditingController searchController = TextEditingController();
   int _selectedIndex = 0;
   final scafoldKey = GlobalKey<ScaffoldState>();
-
-  List<dynamic> _pages(BuildContext context) {
-    return [
-      HomeScreenBody(),
-      const AnalyticsScreen(),
-      const FavoriteScreen(),
-      const WalletScreen(),
-    ];
-  }
-
+  static final List<Widget> _pages = [
+    const HomeScreenBody(),
+    const AnalyticsScreen(),
+    const FavoriteScreen(),
+    const WalletScreen(),
+  ];
   void navigatoTo(int index) {
     setState(() {
       _selectedIndex = index;
@@ -41,15 +39,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // final displayData = Provider.of<AuthenticationProvider>(context);
     SizeConfig.init(context);
     return Scaffold(
       key: scafoldKey,
-      appBar: appBar(context, scafoldKey),
+      // backgroundColor: Colors.brown,
+      // backgroundColor: Colors.white,
+      appBar: appBar(context,scafoldKey),
       drawer: const AppDrawer(),
-      body: SafeArea(
-        child: _pages(context)[_selectedIndex],
-      ),
+      body: _pages[_selectedIndex],
       bottomNavigationBar:
           NavBar(navigatoTo: navigatoTo, selectedIndex: _selectedIndex),
     );
